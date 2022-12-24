c9d1faf489a45920c94478350b0f3641646ad4d6
Enjoyer-Chad <kolabagas@gmail.com>
2022-12-24 19:12:26 +0200

    v2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите количество чисел");
+        Massive.numbers = sc.nextInt();
+        System.out.println("Введите числа");
+        for (int i = 0; i < Massive.numbersStorage.length; i++){
+            Massive.numbersStorage[i] = sc.nextInt();
+        }
     }
 }
---
+++ src/Massive.java
@@ -0,0 +1,4 @@
+public class Massive {
+    public static int numbers;
+    public static int[] numbersStorage = new int[numbers];
+}
