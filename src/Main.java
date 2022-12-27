6e4c147b3c159ec68563063505e47dd3e33bfdc8
Enjoyer-Chad <kolabagas@gmail.com>
2022-12-27 17:12:32 +0200

    v3

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Введите количество чисел");
-        Massive.numbers = sc.nextInt();
-        System.out.println("Введите числа");
-        for (int i = 0; i < Massive.numbersStorage.length; i++){
-            Massive.numbersStorage[i] = sc.nextInt();
+        Massive ms = new Massive(sc.nextInt());
+        ms.numbersTaker(ms.theNumber);
+        System.out.println("Выберите необходимую операцию\n1.Суммирование\n2.Умножение\n3.Поделить\n4.Вычитание");
+        Massive.userChoice = sc.nextInt();
+        System.out.println("Введите диапазон чисел (первое значение имеет индекс 0");
+        if (Massive.userChoice == ms.SUM) {
+            ms.sum(sc.nextInt(), sc.nextInt());}
+        if (Massive.userChoice == ms.MULTIPLICATION) {
+            ms.multiplication(sc.nextInt(), sc.nextInt());
+        }if (Massive.userChoice == ms.DIVIDE) {
+            ms.divide(sc.nextInt(), sc.nextInt());
+        }if (Massive.userChoice == ms.SUBTRACTION) {
+            ms.subtraction(sc.nextInt(), sc.nextInt());}
         }
     }
-}
+
--- src/Massive.java
+++ src/Massive.java
@@ -1,4 +1,50 @@
+import java.util.Scanner;
 public class Massive {
-    public static int numbers;
-    public static int[] numbersStorage = new int[numbers];
+    int SUM = 1;
+    int MULTIPLICATION = 2;
+    int DIVIDE = 3;
+    int SUBTRACTION = 4;
+    Scanner sc = new Scanner(System.in);
+    static int userChoice;
+    int theNumber;
+    public int[] numbersStorage;
+    public Massive(int number) {
+        this.theNumber = number;
+        numbersStorage = new int[theNumber];
+    }
+    public void numbersTaker(int a) {
+        System.out.println("Введите числа");
+        for (int i = 0; i < a; i++) {
+            numbersStorage[i] = sc.nextInt();
+        }
+    }
+
+    void divide(int a , int b) {
+        double divider = numbersStorage[a];
+        for (int c = a; c < b; c++) {
+            divider = divider / numbersStorage[c + 1];
+        }
+        System.out.println(divider);
+    } // Dividing methode for chosen numbers
+    void sum(int a, int b) {
+        double sumNumbers = numbersStorage[a];
+        for (int c = a; c < b; c++) {
+            sumNumbers = sumNumbers + numbersStorage[a];
+        }
+        System.out.println(sumNumbers);
+    } // Sum methode for chosen numbers
+    void multiplication(int a, int b) {
+        double multiplication = numbersStorage[a];
+        for (int c = a; c < b; c++) {
+            multiplication = multiplication * numbersStorage[c+1];
+        }
+        System.out.println(multiplication);
+    } // Multiplication methode for chosen numbers
+    void subtraction(int a, int b) {
+        double subtract = 0;
+        for (int c = a; c < b; c++) {
+            subtract = subtract * numbersStorage[c];
+        }
+        System.out.println(subtract);
+    }  // Subtraction methode for chosen numbers
 }
