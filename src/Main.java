7198785c0556ba561f1234bb014394e4886fac3a
kolab <kolabagas@gmail.com>
2022-12-30 06:39:15 +0200

    v3.1

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,7 @@ public class Main {
         System.out.println("Выберите необходимую операцию\n1.Суммирование\n2.Умножение\n3.Поделить\n4.Вычитание");
         Massive.userChoice = sc.nextInt();
         System.out.println("Введите диапазон чисел (первое значение имеет индекс 0");
-        if (Massive.userChoice == ms.SUM) {
-            ms.sum(sc.nextInt(), sc.nextInt());}
-        if (Massive.userChoice == ms.MULTIPLICATION) {
-            ms.multiplication(sc.nextInt(), sc.nextInt());
-        }if (Massive.userChoice == ms.DIVIDE) {
-            ms.divide(sc.nextInt(), sc.nextInt());
-        }if (Massive.userChoice == ms.SUBTRACTION) {
-            ms.subtraction(sc.nextInt(), sc.nextInt());}
+        ms.test();
         }
     }
 
--- src/Massive.java
+++ src/Massive.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
+
 public class Massive {
     int SUM = 1;
     int MULTIPLICATION = 2;
     int DIVIDE = 3;
     int SUBTRACTION = 4;
     Scanner sc = new Scanner(System.in);
-    static int userChoice;
+    public static int userChoice;
     int theNumber;
     public int[] numbersStorage;
+
     public Massive(int number) {
         this.theNumber = number;
         numbersStorage = new int[theNumber];
     }
+
     public void numbersTaker(int a) {
         System.out.println("Введите числа");
         for (int i = 0; i < a; i++) {
@@ -19,27 +22,29 @@ public class Massive {
         }
     }
 
-    void divide(int a , int b) {
+    void divide(int a, int b) {
         double divider = numbersStorage[a];
         for (int c = a; c < b; c++) {
             divider = divider / numbersStorage[c + 1];
         }
         System.out.println(divider);
     } // Dividing methode for chosen numbers
+
     void sum(int a, int b) {
         double sumNumbers = numbersStorage[a];
         for (int c = a; c < b; c++) {
-            sumNumbers = sumNumbers + numbersStorage[a];
+            sumNumbers = sumNumbers + numbersStorage[c+1];
         }
         System.out.println(sumNumbers);
     } // Sum methode for chosen numbers
     void multiplication(int a, int b) {
         double multiplication = numbersStorage[a];
         for (int c = a; c < b; c++) {
-            multiplication = multiplication * numbersStorage[c+1];
+            multiplication = multiplication * numbersStorage[c + 1];
         }
         System.out.println(multiplication);
     } // Multiplication methode for chosen numbers
+
     void subtraction(int a, int b) {
         double subtract = 0;
         for (int c = a; c < b; c++) {
@@ -47,4 +52,18 @@ public class Massive {
         }
         System.out.println(subtract);
     }  // Subtraction methode for chosen numbers
+    void test() {
+        if (userChoice == SUM) {
+            sum(sc.nextInt(), sc.nextInt());
+        }
+        if (userChoice == MULTIPLICATION) {
+            multiplication(sc.nextInt(), sc.nextInt());
+        }
+        if (userChoice == DIVIDE) {
+            divide(sc.nextInt(), sc.nextInt());
+        }
+        if (userChoice == SUBTRACTION) {
+            subtraction(sc.nextInt(), sc.nextInt());
+        }
+    }
 }
